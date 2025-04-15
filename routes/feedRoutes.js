201f3538eb100e8480cd93bcc5d0121fa7f7150c
TechTech_Dev <102860258+TusharTemgire@users.noreply.github.com>
2025-04-15 10:58:16 +0530

    fixed

--- routes/feedRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect } = require('../middleware/authMiddleware');
-const { getFeed, getExplore } = require('../controllers/feedController');
-
-router.get('/', protect, getFeed);
-router.get('/explore', protect, getExplore);
-
-module.exports = router;
--- routes/notificationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect } = require('../middleware/authMiddleware');
-const { getNotifications, markAsRead } = require('../controllers/notificationController');
-
-router.get('/', protect, getNotifications);
-router.patch('/read', protect, markAsRead);
-
-module.exports = router;
